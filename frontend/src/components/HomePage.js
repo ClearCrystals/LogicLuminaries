25164c932bcf265779222437d0fc7c14a22868ef
ClearCrystals <gesparkles@gmail.com>
2024-03-05 22:35:20 -0600

    Created a HomePage

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Sudoku App</h1>
+      </header>
+      <main>
+        <p>Welcome to the home page! This is a basic example of a React project.</p>
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
