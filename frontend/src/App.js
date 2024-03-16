03b5a05409fc96c61bbfb7b7be98e8a81203d1a4
Grace <gsteinmetz@wisc.edu>
2024-03-16 18:18:43 -0500

    screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import HomePage from './components/HomePage'; // Make sure this path is correct
-import Auth from "./components/Auth"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from './components/HomePage'; // Adjust this path as necessary
+import Auth from "./components/Auth";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/components/auth" />} />
         <Route path="/components/auth" element={<Auth />} />
       </Routes>
     </BrowserRouter>
