229eb0cb3713e9880c17f91a750ae8389ea7bb48
Grace <gsteinmetz@wisc.edu>
2024-03-16 18:08:18 -0500

    adding docker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from './components/HomePage'; // Make sure this path is correct
+import Auth from "./components/Auth"
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/components/auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
