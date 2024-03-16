ce7cf8d03c14c773866b91cefda153e9f6e6a367
Nima Karbassi <nkarbassi@wisc.edu>
2024-03-16 01:25:44 -0500

    Add tests and sudoku structure

---
+++ backend/sudoku/sudoku.py
@@ -0,0 +1,13 @@
+class Sudoku:
+
+    def __init__(self, difficulty="Medium"):
+        pass
+
+    def generate_sudoku(self):
+        pass
+
+    def sudoku_status(self):
+        pass
+
+    def solve_sudoku(self):
+        pass
--- backend/sudoku/tests.py
+++ backend/sudoku/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from .sudoku import Sudoku
 
-# Create your tests here.
+class SudokuAlgoTests(TestCase):
+    def test_sudoku_status(self):
+        # Check the sudoku_status for a completed board
+        sudoku = Sudoku()
+        completed_board = [
+            [5, 3, 4, 6, 7, 8, 9, 1, 2],
+            [6, 7, 2, 1, 9, 5, 3, 4, 8],
+            [1, 9, 8, 3, 4, 2, 5, 6, 7],
+            [8, 5, 9, 7, 6, 1, 4, 2, 3],
+            [4, 2, 6, 8, 5, 3, 7, 9, 1],
+            [7, 1, 3, 9, 2, 4, 8, 5, 6],
+            [9, 6, 1, 5, 3, 7, 2, 8, 4],
+            [2, 8, 7, 4, 1, 9, 6, 3, 5],
+            [3, 4, 5, 2, 8, 6, 1, 7, 9]
+        ]
+        self.assertTrue(sudoku.sudoku_status(completed_board))
+
+        # Check for an incomplete board
+        incomplete_board = [
+            [5, 3, 4, 6, 7, 8, 9, 1, 2],
+            [6, 7, 2, 1, 9, 5, 3, 4, 8],
+            [1, 9, 8, 3, 4, 2, 5, 6, 7],
+            [8, 5, 9, 7, 6, 1, 4, 2, 3],
+            [4, 2, 6, 8, 5, 3, 7, 9, 1],
+            [7, 1, 3, 9, 2, 4, 8, 5, 6],
+            [9, 6, 1, 5, 3, 7, 2, 8, 4],
+            [2, 8, 7, 4, 1, 9, 6, 3, 5],
+            [3, 4, 5, 2, 8, 6, 0, 7, 9]
+        ]
+        self.assertFalse(sudoku.sudoku_status(incomplete_board))
+
+    def test_generate_sudoku(self):
+        # Check that board is 9x9
+        sudoku = Sudoku()
+        board = sudoku.generate_sudoku('easy')
+        self.assertEqual(len(board), 9)
+        for row in board:
+            self.assertEqual(len(row), 9)
+
+    def test_solve_sudoku(self):
+        # Check that the solution is a valid
+        sudoku = Sudoku()
+        test_board = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
+        solution = sudoku.solve_sudoku(test_board)
+        self.assertTrue(sudoku.sudoku_status(solution))
