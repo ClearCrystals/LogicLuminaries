0915210e1e86de135186de589c51f92a1495a68d
cmrichmond2002 <cmrichmond@wisc.edu>
2024-03-16 18:50:18 -0500

    Reformated files

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS: List[str] = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'sudoku.apps.SudokuConfig'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "sudoku.apps.SudokuConfig",
 ]
 
 MIDDLEWARE = [
--- backend/sudoku/migrations/0001_initial.py
+++ backend/sudoku/migrations/0001_initial.py
@@ -8,28 +8,35 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Users',
+            name="Users",
             fields=[
-                ('id', models.CharField(max_length=30, primary_key=True, serialize=False)),
-                ('pwd', models.CharField(max_length=30)),
-                ('token', models.CharField(max_length=200)),
+                (
+                    "id",
+                    models.CharField(max_length=30, primary_key=True, serialize=False),
+                ),
+                ("pwd", models.CharField(max_length=30)),
+                ("token", models.CharField(max_length=200)),
             ],
         ),
         migrations.CreateModel(
-            name='Boards',
+            name="Boards",
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False)),
-                ('state', models.CharField(max_length=200)),
-                ('answer', models.CharField(max_length=200)),
-                ('difficulty', models.CharField(max_length=30)),
-                ('style', models.CharField(max_length=30)),
-                ('isFinished', models.IntegerField(default=0)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='sudoku.users')),
+                ("id", models.AutoField(primary_key=True, serialize=False)),
+                ("state", models.CharField(max_length=200)),
+                ("answer", models.CharField(max_length=200)),
+                ("difficulty", models.CharField(max_length=30)),
+                ("style", models.CharField(max_length=30)),
+                ("isFinished", models.IntegerField(default=0)),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="sudoku.users"
+                    ),
+                ),
             ],
         ),
     ]
