f60a5e76bc18e077ee65a8ece06950419793582b
Elizabella Rosenberg <erosenberg6@wisc.edu>
2024-04-10 07:34:58 -0500

    pushing an image to the repo to see if that changes src

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 /**
  * HomePage component serving as the landing page of the Sudoku App.
@@ -14,18 +18,29 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
-    <div>
-      <header>
-        <h1>Sudoku App</h1>
-      </header>
-      <main>
-        <p>Welcome to the home page! This is a basic example of a React project.</p>
-        <div className="btn-group" role="group" aria-label="Authentication Buttons">
-          <Link to="/components/auth?mode=signup" className="btn btn-primary">Signup</Link>
-          <Link to="/components/auth?mode=signin" className="btn btn-primary">Signin</Link>
+    <Container>
+      <Row>
+        <Col><Image src="frontend\public\free_sudoku.jpg" alt="board image" fluid /></Col>
+        <Col>
+        {
+        <div>
+          <header>
+            <h1>Sudoku App</h1>
+          </header>
+          <main>
+            <h2>Welcome to Sudoku<br></br></h2>
+            <h5>Solve, save, and explore new board styles in one place.<br></br></h5>
+            <h5>Register or Login to begin.<br></br></h5>
+            <div className="btn-group" role="group" aria-label="Authentication Buttons">
+              <Link to="/components/auth?mode=signup" className="btn btn-primary">Register</Link>
+              <Link to="/components/auth?mode=signin" className="btn btn-primary">Login</Link>
+            </div>
+          </main>
         </div>
-      </main>
-    </div>
+        }
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
